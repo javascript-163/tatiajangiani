23a34939eb52331e1db0d8b9cf4d07a49a5cf8ef
tatiajangiani <153165677+tatiajangiani@users.noreply.github.com>
2024-01-25 00:32:46 +0400

    end first homework

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+let name = prompt("What is your name? ");
+let surname = prompt("What is your surname? ");
+let year = prompt("what year were you born ");
+let liveplace = prompt("Where do you live? ");
+
+let age = 2024 - year;
+
+alert("Hello " + name + " " + surname + "I am glad that you are " + year + "and live in" + liveplace + "." )
+
