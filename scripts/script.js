ef6219db47f14e1bfaf8f92da1b538f74869e8df
tatiajangiani <153165677+tatiajangiani@users.noreply.github.com>
2024-01-26 20:29:45 +0400

    end

---
+++ scripts/script.js
@@ -0,0 +1,49 @@
+// exercise1
+
+const myFriends = { "Ana", "Elene", "Mariami", "Keta" }
+console.log(Array.isArray(myFriends))
+
+
+// exercise2
+
+console.log(myFriends{ 0})
+
+
+// exercise3
+console.log(myFriends[myFriends.length - 1])
+
+
+// exercise4
+color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
+o = ["th", "st", "nd", "rd"]
+
+let firstColor = color[0];
+let firstO = o[1];
+console.log("1" + firstO + " choice is " + firstColor);
+
+
+let secondColor = color[1];
+let secondO = o[2];
+console.log("2" + secondO + " choice is " + secondColor);
+
+let thirdColor = color[2];
+let thirdO = o[3];
+console.log("3" + thirdO + " choice is " + thirdColor);
+
+
+
+// exercise5
+const Students = {
+    name: "Tatia",
+    surname: "Jangiani",
+    age: 20
+}
+
+Students.hobby = "Tennis";
+console.log(Students)
+
+delete.Students.surname
+console.log(Students)
+
+Students.name = "Mariami";
+console.log(Students)
