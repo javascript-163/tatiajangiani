b569be5f226a0c7aa0a57550d5e9a2ac31a36993
tatiajangiani <153165677+tatiajangiani@users.noreply.github.com>
2024-02-07 19:34:47 +0400

    last change

---
+++ workshop5/scripts/highOrderFunctions.js
@@ -0,0 +1,16 @@
+const numbers = [1,2,3,4,5];
+
+const doubledNumbers = numbers.map(num => num * 2);
+
+const result = [];
+
+for (let number = 0; number < numbers.length; number++ ) {
+    result.push(numbers[number] * 2)
+   
+}
+
+console.log(result)
+
+
+
+
